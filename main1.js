94263108a4b91167aeee53d0f43b6941b9ef8334
zoeng6 <74288736+zoeng6@users.noreply.github.com>
2025-06-04 22:00:38 -0400

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,8 +1,15 @@
 import * as myAccount from "./utilities1.js"
 
-var User = "Zoe"
-myAccount.welcomeUser(User);
-console.log("My balance : " + myAccount.checkBalance(User));
-console.log("Your new balance after deposit : " + myAccount.deposit(200,User));
-console.log("Your new balance after withdraw: " + myAccount.withdraw(20,User));
+var fName = "Zoe"
+var lName = "Ng"
+myAccount.welcomeUser(fName, lName);
+
+console.log("My balance : " + myAccount.checkBalance(fName));
+var CAD = 1050.20
+console.log(`If you convert ${CAD} CAD to USD, you will get ${myAccount.currencyConverter(CAD)} USD.`);
+var principal = 1000
+var time = 2
+var rate = 0.05
+console.log(`If you have a GIC deposit of ${principal} CAD for ${time} years, you will have a total return of ${myAccount.gicDeposit(principal,rate,time)} CAD.`);
+
 
